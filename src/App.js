509437e4cc498486592dc04e3734824307607562
sara-yaa <57491957+sarayaari@users.noreply.github.com>
2023-05-15 10:03:56 +0300

    secondLesson7

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AppExschange from "./comps/appExschange.js";
 function App() {
   return (
     <div className="container">
+    <h1>lesson 7  vsc to git</h1>
     <p> writen by sari</p>
     <AppExschange></AppExschange>
     </div>
